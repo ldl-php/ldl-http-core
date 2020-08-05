8e32c220da4dfc69e90104dea363242c78da92d1
Federico <jpfstange@gmail.com>
2020-08-05 18:15:47 -0300

    set constants to public

--- src/Http/Core/Request/RequestInterface.php
+++ src/Http/Core/Request/RequestInterface.php
@@ -7,6 +7,17 @@ use Symfony\Component\HttpFoundation\ParameterBag;
 
 interface RequestInterface
 {
+    public const HTTP_METHOD_HEAD = 'HEAD';
+    public const HTTP_METHOD_GET = 'GET';
+    public const HTTP_METHOD_POST = 'POST';
+    public const HTTP_METHOD_PUT = 'PUT';
+    public const HTTP_METHOD_PATCH = 'PATCH';
+    public const HTTP_METHOD_DELETE = 'DELETE';
+    public const HTTP_METHOD_PURGE = 'PURGE';
+    public const HTTP_METHOD_OPTIONS = 'OPTIONS';
+    public const HTTP_METHOD_TRACE = 'TRACE';
+    public const HTTP_METHOD_CONNECT = 'CONNECT';
+
     /**
      * Checks if a request is of pre flight type (OPTIONS).
      */
@@ -31,12 +42,65 @@ interface RequestInterface
     public function get(string $variable, $default = null);
 
     /**
+     * @return bool
+     */
+    public function isHttpMethodValid(): bool;
+
+    /**
      * Returns contents of the request body.
      *
      * @return mixed
      */
     public function getContent();
 
+    /**
+     * @return ParameterBag
+     */
     public function getQuery(): ParameterBag;
+
+    /**
+     * @return bool
+     */
+    public function isHead(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isGet(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isPost(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isPut(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isPatch(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isDelete(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isPurge(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isTrace(): bool;
+
+    /**
+     * @return bool
+     */
+    public function isConnect(): bool;
 
 }
